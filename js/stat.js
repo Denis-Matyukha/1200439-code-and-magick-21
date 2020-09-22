ea179d8a070c5790ceaa3a8c150c8ab0df284e6f
Denis-Matyukha <denmatuhawork@gmail.com>
2020-09-22 16:29:22 +0300

    Task is completed but code not clean

--- js/stat.js
+++ js/stat.js
@@ -10,8 +10,10 @@ const CLOUD_Y = 10;
 const GAP = 10;
 const FONT_GAP = 15;
 const TEXT_WIDTH = 50;
-const BAR_HEIGHT = 20;
-const BAR_WIDTH = CLOUD_WIDTH - GAP - TEXT_WIDTH - GAP;
+// const BAR_HEIGHT = 20;
+const BAR_HEIGHT = CLOUD_HEIGHT - GAP * 4 - TEXT_WIDTH * 2;
+// const BAR_WIDTH = CLOUD_WIDTH - GAP - TEXT_WIDTH - GAP;
+const BAR_WIDTH = 50;
 
 
 
@@ -54,7 +56,15 @@ const getBlueShadeHEX = function () {
   return blueShade;
 };
 
-
+const getChartColor = function(player) {
+  let chartColor = "";
+  if (player === "Вы") {
+    chartColor = "#ff0000";
+  } else {
+    chartColor = getBlueShadeHEX();
+  }
+  return chartColor;
+}
 
 
 window.renderStatistics = function (ctx, players, times) {
@@ -95,20 +105,92 @@ window.renderStatistics = function (ctx, players, times) {
 
   for (let i = 0; i < players.length; i++) {
     ctx.fillText(
-      players[i],
+      //________________________________________________________________
+      //убрать ндекс игрока на следующей команде
+      `${i}`+players[i]+Math.floor(times[i]),
       // CLOUD_X + GAP,
-      CLOUD_X + GAP * 4 + FONT_GAP + (GAP * 7 + BAR_HEIGHT) * i,
+      CLOUD_X + GAP + FONT_GAP + (GAP * 4 + BAR_WIDTH) * i,
       // CLOUD_Y + GAP * 10 + FONT_GAP + (GAP + BAR_HEIGHT) * i
       // CLOUD_Y + GAP
-      CLOUD_HEIGHT - GAP * 2
+      CLOUD_Y + GAP * 24
     );
+
+
+      //здесть генерим цвет для всех, кроме одного игрока
+    // ctx.fillStyle = getBlueShadeHEX();
+
+
+    ctx.fillStyle = getChartColor(players[i]);
     ctx.fillRect(
-      CLOUD_X + GAP + TEXT_WIDTH,
+
+
+      // CLOUD_X + GAP + TEXT_WIDTH,
+      CLOUD_X + GAP + FONT_GAP + (GAP * 4 + BAR_WIDTH) * i,
       // CLOUD_X + GAP * 10 + (GAP + BAR_HEIGHT) * i,
-      CLOUD_Y + GAP * 10 + (GAP + BAR_HEIGHT) * i,
+
+
+      // CLOUD_Y + GAP * 10 + (GAP + BAR_HEIGHT) * i,
+      // CLOUD_Y + GAP * 17.5 + TEXT_WIDTH,
+      CLOUD_Y + GAP * 17 + TEXT_WIDTH,
+      //переосмыслить наверху
       // CLOUD_Y + GAP + TEXT_WIDTH,
-      (BAR_WIDTH * times[i]) / maxTime,
-      BAR_HEIGHT
+      // CLOUD_Y + GAP + TEXT_WIDTH,
+
+
+      // (BAR_WIDTH * times[i]) / maxTime,
+      BAR_WIDTH,
+
+
+      // BAR_HEIGHT
+      -((BAR_HEIGHT * times[i]) / maxTime)
     );
+
+      //времено
+      // ctx.fillStyle = '#fff';
+
+    ctx.fillStyle = '#000';
+    ctx.fillText(
+      //________________________________________________________________
+      //убрать ндекс игрока на следующей команде
+      // players[i]+`_${i}`,
+      // Math.floor(times[i])+"____",
+      Math.floor(times[i]),
+      // CLOUD_X + GAP,
+      CLOUD_X + GAP + FONT_GAP + (GAP * 4 + BAR_WIDTH) * i,
+      // CLOUD_Y + GAP * 10 + FONT_GAP + (GAP + BAR_HEIGHT) * i
+      // CLOUD_Y + GAP
+      // CLOUD_Y + GAP * 24
+      // ((BAR_HEIGHT * times[i]) / maxTime)
+      //CLOUD_Y + GAP * 15 + TEXT_WIDTH,
+      // ((BAR_HEIGHT * times[i]) / maxTime)
+      // 70
+      CLOUD_HEIGHT - GAP * 6  - ((BAR_HEIGHT * times[i]) / maxTime)
+    );
+
   }
+
 };
+/*
+
+const CLOUD_WIDTH = 420;
+const CLOUD_HEIGHT = 270;
+const CLOUD_X = 100;
+const CLOUD_Y = 10;
+const GAP = 10;
+const FONT_GAP = 15;
+const TEXT_WIDTH = 50;
+const BAR_HEIGHT = CLOUD_HEIGHT - GAP * 4 - TEXT_WIDTH * 2;
+const BAR_WIDTH = 50;
+
+
+
+const CLOUD_WIDTH = 420;
+const CLOUD_HEIGHT = 270;
+const CLOUD_X = 100;
+const CLOUD_Y = 10;
+const GAP = 10;
+const FONT_GAP = 15;
+const TEXT_WIDTH = 50;
+const BAR_HEIGHT = 20;
+const BAR_WIDTH = CLOUD_WIDTH - GAP - TEXT_WIDTH - GAP;
+*/
